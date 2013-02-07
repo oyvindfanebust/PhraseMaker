0158d97cbd68bcf3d898dd06428c61d4fa6765b7
104171oyfa <oyvind.fanebust@frende.no>
2013-02-07 12:32:24 +0100

    Added console app

---
+++ PhraseMaker.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PhraseMaker.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                System.Console.WriteLine("Please supply a path to a word-store");
+                return;
+            }
+
+            var factory = new PhraseMakerFactory();
+            var path = args[0];
+            PhraseMaker phraseMaker = factory.Create(path);
+            System.Console.WriteLine("Press 'q' to exit.");
+            ConsoleKeyInfo key = System.Console.ReadKey();
+            while(key.KeyChar != 'q')
+            {
+                System.Console.WriteLine(phraseMaker.GeneratePhrase());
+                key = System.Console.ReadKey();
+            }
+            
+        }
+    }
+}
