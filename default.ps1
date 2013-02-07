b578785ffde061003c5041491e31bd2d5262fdd0
104171oyfa <oyvind.fanebust@frende.no>
2013-02-07 13:08:08 +0100

    Build script for creating nuget packages

--- default.ps1
+++ default.ps1
@@ -4,12 +4,18 @@ properties {
   $pwd = Split-Path $psake.build_script_file	
   $configuration = "Release"
   $buildDir = "$pwd\Build\"
+  $version = "1.0.0"
 }
 
 task default -depends Build
 
 task Build {
+  Exec { msbuild "PhraseMaker.sln" /t:Build /p:Configuration=$configuration }
+}
+
+task CreatePackage {
   Exec { msbuild "PhraseMaker.sln" /t:Build /p:Configuration=$configuration /p:OutputPath=$buildDir }
+  Exec { .nuget\nuget.exe pack PhraseMaker.nuspec -Version $version }
 }
 
 task ? -Description "Helper to display task info" {
